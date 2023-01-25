fd30c00443ce4cefe549b1cbe0f0b34159df3bef
Sckramr <kramers4@vcu.edu>
2023-01-24 20:15:09 -0500

    adding terrible plot

---
+++ R/1_clean-data.R
@@ -0,0 +1,21 @@
+#
+# CLEAN DATA
+#
+
+
+library('tidyverse')
+
+
+attach(faithful)
+
+
+faithful2 <- faithful %>% 
+  as_tibble() %>% 
+  mutate(waiting.minute = waiting / 60)
+
+fs::dir_create('app/data')
+
+
+
+
+saveRDS(faithful2, "app/data/faithful2.rds")
